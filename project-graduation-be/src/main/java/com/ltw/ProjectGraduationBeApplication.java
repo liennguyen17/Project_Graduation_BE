f862fc59c9fef3488c871d9c32d12e99dc2ad5e0
liennguyen17 <nguyenthilien12a8bn@gmail.com>
2024-02-20 10:09:31 +0700

    set up project

---
+++ project-graduation-be/src/main/java/com/ltw/ProjectGraduationBeApplication.java
@@ -0,0 +1,13 @@
+package com.ltw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectGraduationBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectGraduationBeApplication.class, args);
+	}
+
+}
